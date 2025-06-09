deedf156859276071cb6800fd5b6c1d10f228184
STGRAM\gffd9 <29088981l@gmail.com>
2025-06-09 11:18:45 +0900

    INIT : 백엔드 프로젝트 세팅

---
+++ backend/src/main/kotlin/io/gffd94/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package io.gffd94.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendApplication>(*args)
+}
